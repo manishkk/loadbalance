0e933b685328ce77a076b82c74eacf408d7771f5
Manish Kumar <manishkk@users.noreply.github.com>
2019-08-05 09:13:44 +0300

    Update Load(p) == Avg(p) case

--- src/Random_Edges_Graph.java
+++ src/Random_Edges_Graph.java
@@ -10,17 +10,18 @@ import java.util.Map;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.Set;
-
+//With Potential Function = Individual maximum Load - Individual minimum load
 //without orphan nodes
 public class Random_Edges_Graph
 {
     private Map<Integer, List<Integer>> adjacencyList;
     static List<Float> Load = new LinkedList<Float>();
-   
+    static List<Float> PotentialLoad = new LinkedList<Float>();
+    static List<Float> PotentialSum = new LinkedList<Float>();
+    static List<Float> NewPotentialSum = new LinkedList<Float>();
     public static int v;
     static int neighbors = 0;
-    static float AverageLoad = 0;
-
+    float AvgLoad;
     /**
      * @param v
      */
@@ -52,18 +53,30 @@ public class Random_Edges_Graph
    
     // return average load on vertex
     int neighbourCount;
+    static float potential;
+    
     public float getAverageLoadOnVertex(int vertex, List<Integer> neighbours){
+    	
         neighbourCount = neighbours.size();
         //System.out.println("Vertex " + vertex + " have "+neighbourCount + " neighbors " );
         float totalLoad = Load.get(vertex-1);
         for (int i = 0; i < neighbourCount; i++) {
             totalLoad += Load.get(neighbours.get(i)-1);
+           
         }
+        AvgLoad = totalLoad / (neighbourCount + 1);
         // return (int) Math.round(totalLoad / (neighbourCount + 1)); // TODO: Remove Round
-        System.out.println("Average Load on vertex "+ vertex + " is " +  totalLoad / (neighbourCount + 1));
-        return totalLoad / (neighbourCount + 1); // TODO: Remove Round
+        System.out.println("Average Load on vertex "+ vertex + " is " +  AvgLoad);
+		/*
+		 * potential = Math.abs(Load.get(vertex-1) - AvgLoad);
+		 * System.out.println("Individual Potential VALUE "+ potential);
+		 * PotentialLoad.add(potential);
+		 */
+        return AvgLoad;
     }
    
+   
+    
     // return vertices with less load than average
     public List<Integer> getRecievers(List<Integer> neighbours, float averageLoad) {
         int neighbourCount = neighbours.size();
@@ -108,6 +121,13 @@ public class Random_Edges_Graph
         }
         return true;
     }
+    
+	/*
+	 * public boolean checkFlag(List<Integer> flags) { //
+	 * System.out.println("I am here"); int numberOfVertices = flags.size(); for(int
+	 * vertex = 0; vertex < numberOfVertices; vertex++) { if (flags.get(vertex) !=
+	 * 1) return false; } return true; }
+	 */
     // Generate Random Number
     public static float getRandomIntegerBetweenRange(float min, float max){
 //     float randomValue = ((Math.random()*((max-min)+1))+min);
@@ -143,7 +163,7 @@ public class Random_Edges_Graph
 
             // Generate Random Loads for each vertex
             for (int i = 1; i <= v; i++) {
-                Load.add(getRandomIntegerBetweenRange(2,100));
+                Load.add(getRandomIntegerBetweenRange(2,10000));
                
             }
                        
@@ -151,12 +171,13 @@ public class Random_Edges_Graph
             // System.out.println("The Adjacency List Representation of the random graph is as below: ");
             System.out.println("Maximum Value: " + Collections.max(Load));
             System.out.println("Minimum Value: " + Collections.min(Load));
+            PotentialSum.add(Collections.max(Load) - Collections.min(Load));
             
-            new MyTimerTask();
             int iterationCount = 1;
+            //List<Integer> flags;
             while (iterationCount <= 1000) {
             Map<String, List<Float>> nodePair=new HashMap<String, List<Float>>();
-           
+            	//flags = new LinkedList<Integer>();
                 for (int vertex = 1; vertex <= v; vertex++) {
                 float vertexLoad = Load.get(vertex - 1);
                 List<Integer> MaxEdgeList = null;
@@ -164,7 +185,7 @@ public class Random_Edges_Graph
                     List<Integer> edgeList = reg.getEdge(vertex);
                     System.out.println("Individual load at vertex " + vertex + " is: " + vertexLoad + " :neighbours: " + edgeList);
                     float averageLoad = reg.getAverageLoadOnVertex(vertex, edgeList);
-                   
+                    
                 if (averageLoad < vertexLoad) {
                 MaxEdgeList = reg.getEdge(vertex);
                
@@ -178,7 +199,7 @@ public class Random_Edges_Graph
 //**************Case 1*******************                
                     //System.out.println("averageLoad " + averageLoad);
                
-                    if (averageLoad <= vertexLoad) {
+                if (averageLoad < vertexLoad) {
                     float loadToBeShared = vertexLoad - averageLoad;
                         //System.out.println("loadToBeShared " + loadToBeShared);
                         List<Integer> recievers = reg.getRecievers(MaxEdgeList, averageLoad);
@@ -214,12 +235,13 @@ public class Random_Edges_Graph
                            
                             //Load.set(vertex - 1, vertexLoad - loadToBeSharedPerReciever * recievers.size());
                             //System.out.println("Current load of this node " + (vertexLoad-(loadToBeSharedPerReciever*recievers.size())));
-                        }  
+                        }
+                       // flags.add(0);
                    
                     }
    
 //**************Case 2*******************                    
-                    if (averageLoad > vertexLoad) {
+                	if (averageLoad > vertexLoad) {
                     //System.out.println("Now I will give Proposals to neighbor about my capacity");
                     float loadToBeTaken = averageLoad - vertexLoad;
                     //System.out.println("Total Load to be taken from neighbours "+ loadToBeTaken);
@@ -247,7 +269,65 @@ public class Random_Edges_Graph
                    
                     }
                    
+                   
                     }
+                    
+                	
+                	 if (averageLoad == vertexLoad) {
+                		//Updte code here
+                		 System.out.println("case 3 occured");
+                		 Float localM=0F;
+                		 Integer posLocalM;
+                		 //calculate LocalM from Smax
+                		 for(Integer vertLocalM:MaxEdgeList) {
+                			 List<Integer> vertEdgeList = reg.getEdge(vertLocalM);
+                			 float avgLoadLocalM = reg.getAverageLoadOnVertex(vertLocalM, vertEdgeList);
+                			 if(localM<averageLoad) {
+                				 localM=avgLoadLocalM;
+                				 posLocalM=vertLocalM;
+                			 }
+                			 
+                		 }
+                		 //Chose Alpha from s.t < Local M.
+                		 //For time being asssume Alpha as 0.1
+                		 Float alpha=0.1F;
+                		 Float SumGiversToVertex=0.0F;
+                		 for (Integer k: MaxEdgeList) {
+                			 float vertexLoadLocalM = Load.get(k - 1);
+                			 float averageLoadLocalM = reg.getAverageLoadOnVertex(k, reg.getEdge(k));
+                			 float loadToBeShared = vertexLoadLocalM - averageLoadLocalM;
+                			 List<Integer> recievers = reg.getRecievers(MaxEdgeList, averageLoad);
+                			 float loadToBeSharedPerReciever = loadToBeShared / (recievers.size()+1);
+                			 if (averageLoad < vertexLoad) {
+                                 
+                                     //System.out.println("loadToBeShared " + loadToBeShared);
+                                     
+                                     //recievers.add(vertex);
+                                    // System.out.println("recievers other than me " + recievers);
+                                     if (recievers.size() > 0) {
+                                     
+                                     System.out.println("Node " + vertex + " can take " + (alpha-loadToBeSharedPerReciever) + " load from node " + k);
+                                     SumGiversToVertex+=loadToBeSharedPerReciever;
+                                     }
+                                     String key=vertex+"_"+k;
+                                     if(!nodePair.containsKey(key)) {
+                                     List<Float> loadList=new ArrayList<Float>();
+                                 loadList.add((alpha-loadToBeSharedPerReciever));
+                                 nodePair.put(key,loadList);
+                                     }else {
+                                 List<Float> list = nodePair.get(key);
+                                 list.add((alpha-loadToBeSharedPerReciever));
+                                 nodePair.put(key,list);
+                                 }
+                			 }
+                			 
+                			 
+                		 }
+						 if((SumGiversToVertex*alpha) < localM) {
+							 System.out.println("Its correct its less than localM");
+						 }
+                	 }
+                   
                    
                 }
                 
@@ -278,14 +358,20 @@ public class Random_Edges_Graph
                 System.out.println("New values from node "+(fromNode)+" with load "+Load.get(fromNode-1)+" to node "+(toNode)+ " with load "+Load.get(toNode-1));
                 
                 }
+               
                 System.out.println("==========================================================================");
                 if (reg.validateStabilization()) break;
+                //if (reg.checkFlag(flags)) break;
                 System.out.println("Load after iteration " + iterationCount + ": " + Load);
-               
                 iterationCount++;
                 System.out.println("Maximum Value: " + Collections.max(Load));
                 System.out.println("Minimum Value: " + Collections.min(Load));
-              
+                PotentialSum.add(Collections.max(Load) - Collections.min(Load));
+				/*
+				 * System.out.println("PotentialLoad: " + PotentialLoad); float PotSum =0;
+				 * for(int i=0 ; i < PotentialLoad.size(); i++) { PotSum +=
+				 * PotentialLoad.get(i); } PotentialSum.add(PotSum);
+				 */
             }
 
             System.out.println("=====================================");
@@ -294,9 +380,18 @@ public class Random_Edges_Graph
             System.out.println("Vertex Count: " + v);
             System.out.println("Graph stabilized on iteration: " + iterationCount);
             System.out.println("Load: " + Load);
+			/*
+			 * System.out.println("Sum of PotentialLoad: " + PotentialSum); for(int i=0 ; i
+			 * < PotentialSum.size(); i++) { if ( i== 0) NewPotentialSum.add(i,
+			 * PotentialSum.get(i)); else NewPotentialSum.add(i,
+			 * PotentialSum.get(i)-PotentialSum.get(i-1)); }
+			 * System.out.println("New Sum of PotentialLoad: " + NewPotentialSum);
+			 */
             System.out.println("=====================================");
             System.out.println("Maximum Value: " + Collections.max(Load));
             System.out.println("Minimum Value: " + Collections.min(Load));
+            PotentialSum.add(Collections.max(Load) - Collections.min(Load));
+            System.out.println("Vaule of Extreme Potential Load in Each Iteration: " + PotentialSum);
         } catch (Exception E) {
             System.out.println("Something went wrong: " + E);
         }
